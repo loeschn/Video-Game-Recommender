36ceacb4de2072e235d628ff993417e3b5f8081e
Nick <nickloesch1993@gmail.com>
2018-11-27 13:30:12 -0800

    Made a rough find similar function in ps4 notebook

--- src/get_info.py
+++ src/get_info.py
@@ -1,3 +1,27 @@
+import pymongo
+import requests
+
+class ScrapingException(Exception):
+    pass
+
+def api_request(url, params=None):
+    """Make an IGDB API request."""
+    headers = {'Accept': 'application/json',
+               "user-key": '1cca0ffb6c7bbc063e1ae12727218933'}
+    response = requests.get(url, headers=headers, params=params)
+    if response.status_code != 200:
+        raise ScrapingException(f"Error: status code {response.status_code}\n\nContent:\n{response.content}")
+    return response.json()
+
+
+
+def api_endpoint(endpoint="", endpoint_id=""):
+    """Make a request for an endpoint & endpoint_id.
+        A generalized api request funtion"""
+        
+    url = f"https://api-2445582011268.apicast.io/{endpoint}/{endpoint_id}"
+    return api_request(url)
+
 
 
 def get_game_info(key="name", value=""):
