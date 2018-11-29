4ff17c4cc27a2c8e5c9703219357d9143c6e7fec
Nick <nickloesch1993@gmail.com>
2018-11-29 13:21:43 -0800

    Make function that cleans title of game from mongoDB to one compatible for metacritic url

--- src/metacritic_scrape.py
+++ src/metacritic_scrape.py
@@ -1,25 +1,38 @@
 import requests
-from selenium.webdriver import (Chrome, Firefox)
+from selenium.webdriver import Firefox
+
+def clean_title(title=""):
+    """cleaning the title of the game as given from the mongodb,
+        to something compatibible for metacritic"""
+    empty=[':', ";", ".", ",", "'", "@", "*", "#", "!", "/"]
+    space=["&", "_"]
+
+    title=title.lower()
+
+    title=title.replace("\ufeff", "")
+
+    for element in empty:
+        title=title.replace(element, "")
+
+    for element in space:
+        title=title.replace(element, " ")
+
+    title=title.replace(" - ", "-")
+    title=title.replace(" ", "-")
+
+    return title
 
 def scrape_game_page(platform="playstation-4", title=""):
     """Returns list of  all users who reviewed a game, and a list of scores for the game"""
     browser = Firefox()
 
-    title = title.lower()
-    title=title.replace("-", "")
-    title=title.replace(":", "")
-    title=title.replace("''", "")
-    title=title.replace("&", " ")
-    title=title.replace(".", "")
-
-
-    title=title.replace(" ", "-")
+    game=clean_title(title)
 
     users=[]
     scores_dict={}
 
     for i in range(100):
-        url= f"https://www.metacritic.com/game/{platform}/{title}/user-reviews?page={i}"
+        url= f"https://www.metacritic.com/game/{platform}/{game}/user-reviews?page={i}"
         browser.get(url)
         reviews = browser.find_elements_by_css_selector('li.user_review')
 
