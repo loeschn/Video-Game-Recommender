a1f6c0f03931116abdf400fb862ce535ff432218
Nick <nickloesch1993@gmail.com>
2018-11-28 16:54:38 -0800

    Edit names of functions in metacritic_scrape.py, also function scrape_game_page returns dictionary of scores & users instead of a list of scores

--- src/metacritic_scrape.py
+++ src/metacritic_scrape.py
@@ -1,26 +1,37 @@
 import requests
 from selenium.webdriver import (Chrome, Firefox)
 
-def get_users_scores(title=""):
+def scrape_game_page(title=""):
     """Returns list of  all users who reviewed a game, and a list of scores for the game"""
     browser = Firefox()
 
     title = title.lower()
     title=title.replace(" ", "-")
 
-    scores=[]
+    #scores=[]
     users=[]
-    
+    scores_dict={}
+
     for i in range(100):
         url= f"https://www.metacritic.com/game/playstation-4/{title}/user-reviews?page={i}"
         browser.get(url)
         reviews = browser.find_elements_by_css_selector('li.user_review')
 
         for i in range(len(reviews)):
-            scores.append(int((reviews[i].text).split('\n')[2]))
-            users.append((reviews[i].text).split('\n')[0])
+            #scores.append(int((reviews[i].text).split('\n')[2]))
+            split = (reviews[i].text).split('\n')
+            users.append(split[0])
+            scores_dict[split[0]]=int(split[2])
 
         if len(reviews)==0:
             break
 
-    return scores, users
+    return scores_dict, users
+
+def scrape_user_page(username=""):
+    "Returns list of all games a user has reviewed, and what score they gave the game"
+
+
+    for i in range(100):
+        url= f"https://www.metacritic.com/user/{username}?page={i}"
+        browser.get(url)
