e7c4ca3ed5c5d6f2ed8cb900fb6e1281107a0d60
Nick <nickloesch1993@gmail.com>
2018-11-28 16:14:46 -0800

    Edit metacritic_scrape.py so function iterates overs all pages of reviews

--- src/metacritic_scrape.py
+++ src/metacritic_scrape.py
@@ -2,20 +2,25 @@ import requests
 from selenium.webdriver import (Chrome, Firefox)
 
 def get_users_scores(title=""):
-    """Returns 1 page of user review scoresfor a given title"""
+    """Returns list of  all users who reviewed a game, and a list of scores for the game"""
     browser = Firefox()
 
     title = title.lower()
     title=title.replace(" ", "-")
 
-    url= f"https://www.metacritic.com/game/playstation-4/{title}/user-reviews"
-    browser.get(url)
-    reviews = browser.find_elements_by_css_selector('li.user_review')
-
     scores=[]
     users=[]
+    
+    for i in range(100):
+        url= f"https://www.metacritic.com/game/playstation-4/{title}/user-reviews?page={i}"
+        browser.get(url)
+        reviews = browser.find_elements_by_css_selector('li.user_review')
 
-    for i in range(len(reviews)):
-        scores.append(int((reviews[i].text).split('\n')[2]))
-        users.append((reviews[i].text).split('\n')[0])
+        for i in range(len(reviews)):
+            scores.append(int((reviews[i].text).split('\n')[2]))
+            users.append((reviews[i].text).split('\n')[0])
+
+        if len(reviews)==0:
+            break
+
     return scores, users
