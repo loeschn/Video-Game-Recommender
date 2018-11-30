afd0879d7c58a357aa8eff18cd0857b12b8b29ba
Nick <nickloesch1993@gmail.com>
2018-11-30 09:55:04 -0800

    Add time.sleep to function

--- src/make_preference_matrix.py
+++ src/make_preference_matrix.py
@@ -3,6 +3,8 @@ import pymongo
 from .metacritic_scrape import scrape_game_page
 from .pandas_functions import clean_df
 from selenium.webdriver import Firefox
+import random
+import time
 
 mc = pymongo.MongoClient()
 db = mc['ps4_game_data']
@@ -10,7 +12,7 @@ games = db['games']
 
 
 def get_all_users(db=games):
-    users=[]
+    #users=[]
     games_dict={}
 
     df = pd.DataFrame(list(db.find()))
@@ -20,10 +22,10 @@ def get_all_users(db=games):
     game_titles = list(df.name)
     browser=Firefox()
     for game in game_titles:
-        games_dict[game], game_users = scrape_game_page(title=game, browser=browser)
+        games_dict[game] = scrape_game_page(title=game, browser=browser)
+        time.sleep(random.randint(0,5))
+        #for user in game_users:
+        #    if user not in users:
+        #        users.append(user)
 
-        for user in game_users:
-            if user not in users:
-                users.append(user)
-
-    return games_dict, users
+    return games_dict#, users
