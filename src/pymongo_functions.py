89d16ee5ccc390f5de61f00fab1d0b54b2501c85
Nick <nickloesch1993@gmail.com>
2018-11-30 18:03:56 -0800

    edit db=mc['game_recommender']

--- src/pymongo_functions.py
+++ src/pymongo_functions.py
@@ -1,11 +1,13 @@
 import pymongo
+import time
+import random
+from selenium.webdriver import Firefox
 
 
 mc = pymongo.MongoClient()
-db = mc['ps4_game_data']
+db = mc['game_recommender']
 games = db['games']
 raw_html_coll = db['raw_html']
-
 
 
 def get_game_info(key="name", value=""):
@@ -14,12 +16,11 @@ def get_game_info(key="name", value=""):
 
 def store_html(url, html, coll=raw_html_coll):
     """"Store raw HTML in MongoDB"""
-    coll.update_one(
-        {'url': url, {'url': url, 'html': html} }, upsert=True
-    )
+    coll.delete_many({'url': url})
+    coll.insert_one({'url': url, 'html': html})
 
 def retrieve_html(url, coll=raw_html_coll):
-"""Retrieve page source for a URL from MongoDB"""
+    """Retrieve page source for a URL from MongoDB"""
     retrieved_data = coll.find_one({'url': url})
     if retrieved_data:
         return retrieved_data.get('html')
@@ -27,10 +28,11 @@ def retrieve_html(url, coll=raw_html_coll):
         return None
 
 
-def scrape_page(url, coll=raw_html_coll):
-    html = retrieve_html(url=url, coll=coll):
+def scrape_page(url, coll=raw_html_coll, browser=Firefox()):
+    html = retrieve_html(url=url, coll=coll)
     if html:
         return html
+
     browser.get(url)
     time.sleep(5 + random.random() * 10)  # Wait 5-15 seconds
     if browser.title.startswith('404'):
