84486b79f09ff726b6cbab09186dac4a03850cb9
Nick <nickloesch1993@gmail.com>
2018-12-03 13:44:24 -0800

    remove time.sleep from get_all_users

--- src/make_preference_matrix.py
+++ src/make_preference_matrix.py
@@ -7,12 +7,13 @@ import random
 import time
 
 mc = pymongo.MongoClient()
-db = mc['ps4_game_data']
-games = db['games']
+db =mc['game_recommender']
+omc=pymongo.MongoClient()
+cb = omc['ps4_game_data']
+games = cb['games']
 
 
 def get_all_users(db=games):
-    #users=[]
     games_dict={}
 
     df = pd.DataFrame(list(db.find()))
@@ -23,9 +24,5 @@ def get_all_users(db=games):
     browser=Firefox()
     for game in game_titles:
         games_dict[game] = scrape_game_page(title=game, browser=browser)
-        time.sleep(random.randint(0,5))
-        #for user in game_users:
-        #    if user not in users:
-        #        users.append(user)
-
-    return games_dict#, users
+        
+    return games_dict
